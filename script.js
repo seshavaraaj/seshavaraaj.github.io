d715bd1d9390c64b4e9f97cb015c7557b3315f04
Seshavaraaj Y <67858061+seshavaraaj@users.noreply.github.com>
2025-10-31 00:07:41 +0530

    Remove project click event listener

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const projects = document.querySelectorAll('.project');
-    projects.forEach(project => {
-        project.addEventListener('click', () => {
-            alert('You clicked on a project!');
-        });
-    });
-});
+
